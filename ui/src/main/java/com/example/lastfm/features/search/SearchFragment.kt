8a8c6a805f10cee87e3ec3818cd6eea5bd0caf12
Alastair Craig <ally_craig@hotmail.com>
2020-01-28 17:06:50 +0000

    add searchview

--- ui/src/main/java/com/example/lastfm/features/search/SearchFragment.kt
+++ ui/src/main/java/com/example/lastfm/features/search/SearchFragment.kt
@@ -1,10 +1,15 @@
 package com.example.lastfm.features.search
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import androidx.lifecycle.Observer
 import com.example.lastfm.R
 import com.example.lastfm.base.framework.BaseFragment
+import com.miguelcatalan.materialsearchview.MaterialSearchView
+import kotlinx.android.synthetic.main.fragment_search.*
 
 class SearchFragment : BaseFragment<SearchViewModel>() {
 
@@ -18,14 +23,27 @@ class SearchFragment : BaseFragment<SearchViewModel>() {
 
         viewModel.state.observe(viewLifecycleOwner, Observer {
             when (it) {
+                is SearchViewState.StartScreen -> showStartScreen()
                 is SearchViewState.Loading -> showLoadingScreen()
                 is SearchViewState.Error -> showErrorDialog()
                 is SearchViewState.NoResults -> showEmptyListView()
                 is SearchViewState.DataReady -> showList(it)
             }
         })
+        setHasOptionsMenu(true)
+        setUpSearchView()
+        //viewModel.getData()
+    }
 
-        viewModel.getData()
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_main, menu)
+        val item: MenuItem = menu.findItem(R.id.action_search)
+        search_view.setMenuItem(item)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    private fun showStartScreen() {
+
     }
 
     private fun showList(data: SearchViewState.DataReady) {
@@ -42,5 +60,24 @@ class SearchFragment : BaseFragment<SearchViewModel>() {
 
     private fun showLoadingScreen() {
 
+    }
+
+    private fun setUpSearchView() {
+        search_view.setOnQueryTextListener(object : MaterialSearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                //get data
+                return false
+            }
+
+            override fun onQueryTextChange(query: String): Boolean {
+                if (query.isEmpty()) {
+                    //show start state
+                }
+                if (query.length > 2) {
+                    //get data
+                }
+                return false
+            }
+        })
     }
 }
