22af87652e6fd60f74a3c6b59bfc3dc38e37d44f
Alastair Craig <ally_craig@hotmail.com>
2020-01-29 05:02:40 +0000

    add api and service call

--- data/src/main/java/com/example/data/network/ArtistApi.kt
+++ data/src/main/java/com/example/data/network/ArtistApi.kt
@@ -1,6 +1,7 @@
 package com.example.data.network
 
 import com.example.data.network.models.ArtistSearchResponse
+import com.example.data.network.models.info.InfoResponse
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -8,9 +9,13 @@ import retrofit2.http.Query
 interface ArtistApi {
 
     companion object {
-        const val ENDPOINT = "?method=artist.search"
+        const val SEARCH_ENDPOINT = "?method=artist.search"
+        const val INFO_ENDPOINT = "?method=artist.getinfo"
     }
 
-    @GET(ENDPOINT)
+    @GET(SEARCH_ENDPOINT)
     fun getArtists(@Query("artist") query: String): Single<ArtistSearchResponse>
+
+    @GET(INFO_ENDPOINT)
+    fun getArtistInfo(@Query("artist") artistName: String): Single<InfoResponse>
 }
--- data/src/main/java/com/example/data/network/ArtistService.kt
+++ data/src/main/java/com/example/data/network/ArtistService.kt
@@ -1,6 +1,8 @@
 package com.example.data.network
 
 import com.example.data.network.models.ArtistResponse
+import com.example.data.network.models.info.ArtistInfo
+import com.example.data.network.models.info.InfoResponse
 import io.reactivex.Single
 import javax.inject.Singleton
 
@@ -9,5 +11,9 @@ class ArtistService(private val api: ArtistApi) {
 
     fun getArtists(query: String): Single<List<ArtistResponse>> {
         return api.getArtists(query).map { it.results.artistmatches.artist }
+    }
+
+    fun getArtistInfo(artistName: String) : Single<ArtistInfo>{
+        return api.getArtistInfo(artistName).map { it.artistInfo }
     }
 }
